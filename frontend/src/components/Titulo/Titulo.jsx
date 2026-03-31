266f478d30ea707b003e926f9ddd16c4f3c75bf6
RafaelMelin33 <rafael.melinsenai@gmail.com>
2026-03-31 17:29:49 -0300

    Delete frontend directory
    arquivos desatualizado

--- frontend/src/components/Titulo/Titulo.jsx
+++
@@ -1,9 +0,0 @@
-import css from './Titulo.module.css';
-
-export default function Titulo({texto, estilo}) {
-    return (
-        <p className={css.titulo + " " + css[estilo]}>
-            {texto}
-        </p>
-    )
-}
